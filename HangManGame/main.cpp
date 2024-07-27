123e5221c16c6e53bf8def5c51e0bfa1e3f63e71
ruben <rsala0108@gmail.com>
2024-07-27 09:19:51 -0700

    Initial Commit

---
+++ HangManGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HangManGame
+//
+//  Created by Ruben Salazar on 7/27/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
